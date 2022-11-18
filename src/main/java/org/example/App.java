32ccf09c7918d9fd03d1854ee32862daf26a38ac
andrea.garcia <andreagarcialopez04@gmail.com>
2022-11-18 14:20:56 +0100

    Modificación de importaciones en el main.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,10 +5,7 @@ package org.example;
  *
  */
 
-import java.lang.String;
-import static java.lang.System.out;
-
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
